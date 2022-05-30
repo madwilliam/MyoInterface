4d74ae3fe5bf52263b84fcfa01a3975a798a090e
madwilliam5225 <madwilliam5225@gmail.com>
2022-05-29 17:33:37 -0700

    added readme

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 from MyoInterface import MyoInterface
-import struct
 interface = MyoInterface()
-
 async def function(interface:MyoInterface):
-    # await interface.subscribe_raw_eeg(raw_emgg_callback)
     await interface.stream_raw_emg()
 interface.connect_and_run_function(function)
-print()
